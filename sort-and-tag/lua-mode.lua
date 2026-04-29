17afbf97228c3c2fc28f4c4b6d8636e046c94766
NonlinearFruit <1123benji5813@gmail.com>
2026-04-29 11:27:37 -0600

    Golf sort and tag in lua mode

--- sort-and-tag/lua-mode.lua
+++ sort-and-tag/lua-mode.lua
@@ -2,9 +2,9 @@ v=vim.api
 f=v.nvim_buf_get_lines
 b=f(0,0,-1,0)
 s=f(0,0,-1,0)
-table.sort(s,function(a,b)return a<b end)
+table.sort(s)
 for k,r in pairs(s)do s[r]=k end
-for i,r in ipairs(b)do b[i]=r:sub(1,1)=="_"and r or string.format("[%02d] %s",s[r],r)end
+for i,r in ipairs(b)do b[i]=r:find"_"and r or("[%02d] "..r):format(s[r])end
 v.nvim_buf_set_lines(0,0,-1,0,b)
 vim.cmd.x()
 
